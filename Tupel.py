7ab91563ef773f7d07a6b70da197fccdcd929665
kashif758 <kashifmustari@gmail.com>
2025-12-01 16:45:13 +0600

    Tupel

---
+++ Tupel.py
@@ -0,0 +1,13 @@
+a = (1 ,2, 4, 5, 8, 7, 8, 9)
+
+print(type(a))
+
+b = (1) # This is not a tuple, just an integer
+print(type(b))
+
+c = (1,) # This is a tuple becose of the comma(,)
+'''
+print(type(c))
+
+a[0] = 10 # This will raise an error because tuples are immutable
+'''
---
+++ Tuple Methords.py
@@ -0,0 +1,11 @@
+a = (1 ,2, 4, 5, 8, 7, 8, 5)
+
+'''Tuple have 2 main methords:'''
+# 1 (count):
+print(a.count(8)) # counts how many times 8 appears in the tuple
+
+# 2 (index):
+print(a.index(5)) # returns the index of the first occurrence of 5 in the tuple
+
+
+print(len(a)) # returns the length of the tuple
